fe54e6f4a69be8e9a0d8c060d3ca21088b2abfb6
lquan <1570599338@qq.com>
2022-02-07 10:23:00 +0800

    pag页面重新处理

--- retire/src/main/java/com/lquan/common/page/TableSupport.java
+++ retire/src/main/java/com/lquan/common/page/TableSupport.java
@@ -1,53 +1,31 @@
 package com.lquan.common.page;
 
-
 import com.lquan.common.utils.ServletUtils;
+import com.lquan.common.constant.Constants;
 
 /**
  * 表格数据处理
- *
+ * 
  * @author ruoyi
  */
-public class TableSupport {
-    /**
-     * 当前记录起始索引
-     */
-    public static final String PAGE_NUM = "pageNum";
-
-    /**
-     * 每页显示记录数
-     */
-    public static final String PAGE_SIZE = "pageSize";
-
-    /**
-     * 排序列
-     */
-    public static final String ORDER_BY_COLUMN = "orderByColumn";
-
-    /**
-     * 排序的方向 "desc" 或者 "asc".
-     */
-    public static final String IS_ASC = "isAsc";
-
-    /**
-     * 分页参数合理化
-     */
-    public static final String REASONABLE = "reasonable";
-
+public class TableSupport
+{
     /**
      * 封装分页对象
      */
-    public static PageDomain getPageDomain() {
+    public static PageDomain getPageDomain()
+    {
         PageDomain pageDomain = new PageDomain();
-        pageDomain.setPageNum(ServletUtils.getParameterToInt(PAGE_NUM));
-        pageDomain.setPageSize(ServletUtils.getParameterToInt(PAGE_SIZE));
-        pageDomain.setOrderByColumn(ServletUtils.getParameter(ORDER_BY_COLUMN));
-        pageDomain.setIsAsc(ServletUtils.getParameter(IS_ASC));
-        pageDomain.setReasonable(ServletUtils.getParameterToBool(REASONABLE));
+        pageDomain.setPageNum(ServletUtils.getParameterToInt(Constants.PAGE_NUM));
+        pageDomain.setPageSize(ServletUtils.getParameterToInt(Constants.PAGE_SIZE));
+        pageDomain.setOrderByColumn(ServletUtils.getParameter(Constants.ORDER_BY_COLUMN));
+        pageDomain.setIsAsc(ServletUtils.getParameter(Constants.IS_ASC));
         return pageDomain;
     }
 
-    public static PageDomain buildPageRequest() {
+    public static PageDomain buildPageRequest()
+    {
         return getPageDomain();
     }
+
 }
